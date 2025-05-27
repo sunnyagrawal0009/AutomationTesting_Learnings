0d1b0f1e9e516b5d3ab9b552848d44e51094daa6
sunnyagrawal0009 <sunnyagrawal0009@gmail.com>
2025-05-27 09:53:04 +0530

    AutomationStep5and6

---
+++ AutomationTesting/seleniumtask5and6/NaveenAutomationPractice/src/main/java/com/sun/NaveenAutomationPractice/TestAutomation.java
@@ -0,0 +1,150 @@
+package com.sun.NaveenAutomationPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestAutomation {
+
+	public static void main(String[] args) {
+		/*
+		 * task2(); task3();
+		 */
+		task6();
+	}
+
+	public static void task1() {
+
+		// Automatically manage drivers
+		WebDriverManager.chromedriver().setup();
+
+		/*
+		 * ChromeOptions options = new ChromeOptions();
+		 * options.addArguments("--remote-allow-origins=*");
+		 */
+
+		/* WebDriver driver = new ChromeDriver(options); */
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Title: " + driver.getTitle());
+		driver.quit();
+
+	}
+
+	public static void task2() {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.className("gLFyf")).sendKeys("pentagon");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement searchBtn = wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK")));
+		searchBtn.click();
+		String title = driver.getTitle();
+		if (title.equalsIgnoreCase("pentagon")) {
+			System.out.println("Assertion is true");
+		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+
+	}
+
+	public static void task3() {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.findElement(By.name("username")).sendKeys("student");
+		driver.findElement(By.name("password")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
+		String message = driver.findElement(By.className("post-title")).getText();
+
+		System.out.println("Resultant message1 is:-" + message);
+
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.findElement(By.name("username")).sendKeys("incorrectUser");
+		driver.findElement(By.name("password")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
+		String message2 = driver.findElement(By.id("error")).getText();
+		System.out.println("Resultant message2 is:-" + message2);
+
+		driver.close();
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.quit();
+
+	}
+
+	public static void task4() {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.findElement(By.xpath("//a[text()='Contact']")).click();
+
+		String currentUrl = driver.getCurrentUrl();
+		String expectedURl = "https://practicetestautomation.com/contact/";
+
+		Assert.assertEquals(currentUrl, expectedURl, "Navigation URL mismatch");
+
+	}
+
+	public static void task5() {
+
+		// checkbox
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/checkbox");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		WebElement icon = driver.findElement(By.cssSelector(".rct-checkbox svg"));
+		String iconClass = icon.getAttribute("class");
+
+		if (iconClass.contains("rct-icon-uncheck")) {
+		    icon.click();
+		}
+		driver.close();
+	}
+
+	public static void task6() {
+
+		// radio buttons
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/radio-button");
+		
+		WebElement yesLabel = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+		yesLabel.click();
+		
+		driver.close();
+		
+	}
+
+}
